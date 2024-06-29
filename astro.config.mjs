6a3b8fdbdc2e8c2abe7b6e75b95636cf46b23e56
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2024-06-29 14:49:49 -0400

    Added Netlify adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
